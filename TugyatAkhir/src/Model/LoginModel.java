a264bbb69de76e4f0ade5a2c26e347e65b44e773
mamank slebor <achmadr203@gmail.com>
2024-01-30 15:20:09 +0700

    enkapsulasi

--- TugyatAkhir/src/Model/LoginModel.java
+++ TugyatAkhir/src/Model/LoginModel.java
@@ -1,28 +1,39 @@
 package Model;
 
-
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.util.Objects;
-
 public class LoginModel {
-    public static String USERNAME;
-    protected static boolean loginFlag;
-    public static boolean isAdmin;
-    protected static String fullName;
-    protected static String oldPassword;
-    protected static String phoneNumber;
-    protected static String fullUsername;
+    private static String USERNAME;
+    private static boolean loginFlag;
+    private static boolean isAdmin;
+    private static String fullName;
+    private static String oldPassword;
+    private static String phoneNumber;
+    private static String fullUsername;
 
     // Other methods and properties specific to the model can be added here.
     // For example, methods to handle authentication logic.
 
     public static boolean getLoginFlag() {
         return loginFlag;
+    }
+
+    public static String getUSERNAME() {
+        return USERNAME;
+    }
+
+    public static boolean isAdmin() {
+        return isAdmin;
+    }
+
+    // Metode setter untuk memperbarui nilai variabel
+    public static void setLoginFlag(boolean flag) {
+        loginFlag = flag;
+    }
+
+    public static void setUSERNAME(String username) {
+        USERNAME = username;
+    }
+
+    public static void setAdmin(boolean adminStatus) {
+        isAdmin = adminStatus;
     }
 }
